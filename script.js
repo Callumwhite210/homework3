b8e3b04cfafe1927b2bc0ac0b6aab7c8e7fae8b6
Callum White <callumwhite210@gmail.com>
2020-09-02 15:34:55 +0930

    random password in console

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// counts up from 97 on the charsheet until it reaches 26
+let letters = [...Array(26).keys()].map(i => String.fromCharCode(i + 97)); 
+console.log(letters);
+
+let upperLetters = [...Array(26).keys()].map(i => String.fromCharCode(i + 65));
+console.log(upperLetters);
+
+let number = [1,2,3,4,5,6,7,8,9,0];
+console.log(number);
+
+let special = ['!','@','#','&','%'];
+console.log(special); 
+
+let passwordArray = [
+  letters,
+  upperLetters,
+  number,
+  special
+];
+
+let passwordLength = 4;
+let password = '';
+for(let i =0; i <= passwordLength; i++){
+    let arrayNum = Math.floor(Math.random() * 4); //gives us random number then math.floor rounds it
+    let charNum = Math.floor(Math.random() * 4);
+    console.log(`Array Choice ${arrayNum} char choice ${charNum}`);
+    let passwordChar = passwordArray [arrayNum][charNum];
+    password = password + passwordChar;
+}
+
+console.log(`Password is ${password}`);
+
+// Create functions for random letters
+// Found javascript browser charset on forum: http://www.net-comber.com/charset.html
+// Create promt to set password prefrences with if statements 
+// print password
