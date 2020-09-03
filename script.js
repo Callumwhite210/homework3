83143172be2c1df71b6a654edde482579dfb79dc
Callum White <callumwhite210@gmail.com>
2020-09-03 22:06:52 +0930

    Finished base

--- script.js
+++ script.js
@@ -31,37 +31,50 @@ let special = ['!','@','#','&','%'];
 console.log(special); 
 
 let passwordArray = [
-  letters,
-  upperLetters,
-  number,
-  special
+  
 ];
 //Trents code ends
 function generatePassword() {
   enter = parseInt(prompt("How many characters between 8 and 128?"));
-}
+
 //Validate input
+//continue
 if (enter < 8 || enter > 128) {
   enter = parseInt(prompt("Please input a valid number"));
 }
-//continue 
-
+confirmLower=confirm("would you like letters?");
+if (confirmLower){
+  passwordArray=passwordArray.concat(letters)
+}
+confirmUpper=confirm("would you like upper case letters?");
+if (confirmUpper){
+  passwordArray=passwordArray.concat(upperLetters)
+}
+confirmNumber=confirm("would you like numbers?");
+if (confirmNumber){
+  passwordArray=passwordArray.concat(number)
+}
+confirmSpecial=confirm("would you like special characters?"); 
+if (confirmLower){
+  passwordArray=passwordArray.concat(special)
+}
+//Array choice, turn off array numbers   
 //Trents Code starts again
-let passwordLength = 4;
+let passwordLength = enter;
+console.log(passwordArray);
 let password = '';
 for(let i =0; i <= passwordLength; i++){
-    let arrayNum = Math.floor(Math.random() * 4); //gives us random number then math.floor rounds it
-    let charNum = Math.floor(Math.random() * 4);
-    console.log(`Array Choice ${arrayNum} char choice ${charNum}`);
-    let passwordChar = passwordArray [arrayNum][charNum];
+    let arrayNum = Math.floor(Math.random() * passwordArray.length); //gives us random number then math.floor rounds it
+    console.log(`Array Choice ${arrayNum} char choice`);
+    let passwordChar = passwordArray [arrayNum];
     password = password + passwordChar;
 }
 //Trents code ends
 console.log(`Password is ${password}`);
-
+return password;
+}
 // Create functions for random letters
 // Found javascript browser charset on forum: http://www.net-comber.com/charset.html
 // Create promt to set password prefrences with if statements
 // Randomize 
-// generate
 //print
